7f054227d52b640409f3e3c3f66c45f031934aca
Fahad-CSE16 <Fahad-CSE16>
2020-11-08 18:07:53 +0600

    upto reset

--- session/urls.py
+++ session/urls.py
@@ -1,10 +1,19 @@
 from django.urls import path
 from .views import loginuser,logoutuser,registration,change_password
-app_name='session'
+from django.contrib.auth.views import PasswordResetView,PasswordResetDoneView, PasswordResetConfirmView, PasswordResetCompleteView
+
 urlpatterns = [
     path('login/',loginuser,name="login"),
     path('logout/',logoutuser,name="logout"),
     path('signup/',registration,name="signup"),
     path('password/',change_password,name="password"),
+
+
+    path('reset/password/',PasswordResetView.as_view(template_name='session/reset_pass.html'),name="password_reset"),
+    path('reset/password/done/',PasswordResetDoneView.as_view(template_name='session/reset_pass_done.html'),name="password_reset_done"),
+    path('reset/<uidb64>/<token>/',PasswordResetConfirmView.as_view(template_name='session/password_reset_confirm.html'),name="password_reset_confirm"),
+    path('reset/done/',PasswordResetView.as_view(template_name='session/password_reset_complete.html'),name="password_reset_complete"),
+
+
     
 ]
--- session/views.py
+++ session/views.py
@@ -48,7 +48,7 @@ def registration(request):
             email=EmailMessage(mail_subject,message, to=[send_mail])
             email.send()
             messages.success(request,'Successfully created account')
-            return redirect('session:login') 
+            return redirect('login') 
     else:
         form=SignUpForm()
     return render(request, 'session/signup.html',{'form':form})   
